0704d4c9e1b90de905c9fd20551c72b0711bc19b
Melita <13519063@std.stei.itb.ac.id>
2022-03-18 09:38:20 +0700

    delete picture field

--- lib/model/event.dart
+++ lib/model/event.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 
 Future<String?> addEvent(String tripID, String userID, String caption, String eventDate, String postTime, var file) async {
   var request = http.MultipartRequest('POST', Uri.parse('http://127.0.0.1:3000/addEvent'));
-  request.headers["Content-Type"] = 'application/json';
+  request.headers["Content-Type"] = 'multipart/form-data';
 
   request.fields["tripID"] = tripID;
   request.fields["userID"] = userID;
   request.fields["caption"] = caption;
   request.fields["eventDate"] = eventDate;
   request.fields["postTime"] = postTime;
-  request.fields["picture"] = postTime;
 
   final eventImage = http.MultipartFile.fromBytes('picture', file);
   // final eventImage = await http.MultipartFile.fromPath('picture', file);
@@ -24,6 +25,14 @@ Future<String?> addEvent(String tripID, String userID, String caption, String ev
 
   print("reason: ");
   print(res.reasonPhrase);
-  
-  return res.reasonPhrase;
+  print(res.statusCode);
+  print(res.headers);
+  print(res.request?.headers);
+
+  if(res.statusCode == 200){
+    return "SUCCESS";
+  }
+  else{
+    return res.reasonPhrase;
+  }
 }
