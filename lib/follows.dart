727d61a295aaa9aa2e17e9e41fd0b4d58131de58
Melita <13519063@std.stei.itb.ac.id>
2022-03-28 19:08:16 +0700

    feat: connect follow list with backend done

--- lib/follows.dart
+++ lib/follows.dart
@@ -1,6 +1,6 @@
 import 'package:diorama_id/main.dart';
 import 'package:flutter/material.dart';
-
+import 'model/follows_model.dart';
 class FollowPage extends StatefulWidget {
   const FollowPage({Key? key}) : super(key: key);
 
@@ -10,13 +10,236 @@ class FollowPage extends StatefulWidget {
 
 class _FollowPageState extends State<FollowPage>
     with SingleTickerProviderStateMixin {
-  bool _isFollowing = true;
   late TabController _controller;
+  int _userID = 1; // which user's follow page
+  late Followers _followerList;
+  late Followers _followingList;
+  final followerWidget = <Widget>[];
+  var _followPics = [];
+  final followingWidget = <Widget>[];
+  var _followingPics = [];
+  String _username = "username";
 
   @override
   void initState() {
     super.initState();
     _controller = new TabController(vsync: this, length: 2);
+    fetchFollowers(_userID.toString()).then((list){
+      _followerList = list[0];
+      _followPics = list[1];
+      // print("follow list: "+_followerList.list.length.toString());
+      // print("follow pics list: " + _followPics.length.toString());
+      InitFollowerList();
+      setState(() {});
+    });
+
+    fetchFollowing(_userID.toString()).then((list){
+      _followingList = list[0];
+      _followingPics = list[1];
+      // print("follow list: "+_followingList.list.length.toString());
+      // print("follow pics list: " + _followingPics.length.toString());
+      InitFollowingList();
+      setState(() {});
+    });
+
+    getUserData(_userID.toString()).then((userdata){
+      setState(() {
+        _username = userdata["username"];
+        // print(_username);
+      });
+    });
+  }
+
+  void InitFollowerList() {
+    for(var i=0;i<_followerList.list.length;i++)
+    {
+      followerWidget.add(
+        Stack(children: <Widget>[
+          Container(
+            height: 100,
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+            child: CircleAvatar(
+              radius: 40,
+              backgroundImage: MemoryImage(_followPics[i]),
+              backgroundColor: Colors.transparent,
+            ),
+          ),
+          Container(
+            height: 100,
+            width: double.infinity,
+            child: SizedBox(
+                width: double.infinity,
+                height: double.infinity,
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty
+                        .resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        if (states.contains(
+                                MaterialState.pressed) ||
+                            states.contains(
+                                MaterialState.hovered))
+                          return const Color(0x10000000);
+                        return Colors.transparent;
+                      },
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Container(
+                    height: 100,
+                    width: double.infinity,
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.fromLTRB(100, 8, 4, 8),
+                    child: RichText(
+                      text: TextSpan(
+                        text: _followerList.list[i]['username'],
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16.0),
+                      ),
+                    ),
+                  ),
+                )),
+          ),
+          // Visibility(
+          //   visible: false,
+          //   child: Container(
+          //     height: 100,
+          //     width: double.infinity,
+          //     alignment: Alignment.centerRight,
+          //     padding: EdgeInsets.fromLTRB(4, 8, 15, 8),
+          //     child: TextButton(
+          //       style: TextButton.styleFrom(
+          //         textStyle: const TextStyle(fontSize: 16),
+          //         fixedSize: Size.fromHeight(50),
+          //       ),
+          //       onPressed: () {},
+          //       child: const Text('Follow'),
+          //     ),
+          //   ),
+          // ),
+          // Visibility(
+          //   visible: true,
+          //   child: Container(
+          //     height: 100,
+          //     width: double.infinity,
+          //     alignment: Alignment.centerRight,
+          //     padding: EdgeInsets.fromLTRB(4, 8, 15, 8),
+          //     child: TextButton(
+          //       style: TextButton.styleFrom(
+          //         textStyle: const TextStyle(fontSize: 16),
+          //         fixedSize: Size.fromHeight(50),
+          //       ),
+          //       onPressed: () {
+          //         setState(() {});
+          //       },
+          //       child: Text('Unfollow',
+          //           style: TextStyle(color: Colors.red)),
+          //     ),
+          //   ),
+          // ),
+        ]),
+      );
+    }
+  }
+
+  void InitFollowingList() {
+    for(var i=0;i<_followingList.list.length;i++)
+    {
+      followingWidget.add(
+        Stack(children: <Widget>[
+          Container(
+            height: 100,
+            width: double.infinity,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+            child: CircleAvatar(
+              radius: 40,
+              backgroundImage: MemoryImage(_followingPics[i]),
+              backgroundColor: Colors.transparent,
+            ),
+          ),
+          Container(
+            height: 100,
+            width: double.infinity,
+            child: SizedBox(
+                width: double.infinity,
+                height: double.infinity,
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty
+                        .resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        if (states.contains(
+                                MaterialState.pressed) ||
+                            states.contains(
+                                MaterialState.hovered))
+                          return const Color(0x10000000);
+                        return Colors.transparent;
+                      },
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Container(
+                    height: 100,
+                    width: double.infinity,
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.fromLTRB(100, 8, 4, 8),
+                    child: RichText(
+                      text: TextSpan(
+                        text: _followingList.list[i]['username'],
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16.0),
+                      ),
+                    ),
+                  ),
+                )),
+          ),
+          // Visibility(
+          //   visible: false,
+          //   child: Container(
+          //     height: 100,
+          //     width: double.infinity,
+          //     alignment: Alignment.centerRight,
+          //     padding: EdgeInsets.fromLTRB(4, 8, 15, 8),
+          //     child: TextButton(
+          //       style: TextButton.styleFrom(
+          //         textStyle: const TextStyle(fontSize: 16),
+          //         fixedSize: Size.fromHeight(50),
+          //       ),
+          //       onPressed: () {},
+          //       child: const Text('Follow'),
+          //     ),
+          //   ),
+          // ),
+          // Visibility(
+          //   visible: true,
+          //   child: Container(
+          //     height: 100,
+          //     width: double.infinity,
+          //     alignment: Alignment.centerRight,
+          //     padding: EdgeInsets.fromLTRB(4, 8, 15, 8),
+          //     child: TextButton(
+          //       style: TextButton.styleFrom(
+          //         textStyle: const TextStyle(fontSize: 16),
+          //         fixedSize: Size.fromHeight(50),
+          //       ),
+          //       onPressed: () {
+          //         setState(() {});
+          //       },
+          //       child: Text('Unfollow',
+          //           style: TextStyle(color: Colors.red)),
+          //     ),
+          //   ),
+          // ),
+        ]),
+      );
+    }
   }
 
   @override
@@ -31,7 +254,7 @@ class _FollowPageState extends State<FollowPage>
               Padding(
                 padding: EdgeInsets.all(16.0),
                 child: Text(
-                  'username_user',
+                  _username,
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
                 ),
               ),
@@ -55,153 +278,14 @@ class _FollowPageState extends State<FollowPage>
                   controller: _controller,
                   children: [
                     SingleChildScrollView(
-                      child: Column(children: <Widget>[
-                        Stack(children: <Widget>[
-                          Container(
-                            height: 100,
-                            width: double.infinity,
-                            alignment: Alignment.centerLeft,
-                            padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
-                            child: CircleAvatar(
-                              radius: 40,
-                              backgroundImage: NetworkImage(
-                                  'https://avatars.githubusercontent.com/u/57818885?v=4'),
-                              backgroundColor: Colors.transparent,
-                            ),
-                          ),
-                          Container(
-                            height: 100,
-                            width: double.infinity,
-                            child: SizedBox(
-                                width: double.infinity,
-                                height: double.infinity,
-                                child: TextButton(
-                                  style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty
-                                        .resolveWith<Color>(
-                                      (Set<MaterialState> states) {
-                                        if (states.contains(
-                                                MaterialState.pressed) ||
-                                            states.contains(
-                                                MaterialState.hovered))
-                                          return const Color(0x10000000);
-                                        return Colors.transparent;
-                                      },
-                                    ),
-                                  ),
-                                  onPressed: () {},
-                                  child: Container(
-                                    height: 100,
-                                    width: double.infinity,
-                                    alignment: Alignment.centerLeft,
-                                    padding: EdgeInsets.fromLTRB(100, 8, 4, 8),
-                                    child: RichText(
-                                      text: TextSpan(
-                                        text: 'friend1',
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.w700,
-                                            fontSize: 16.0),
-                                      ),
-                                    ),
-                                  ),
-                                )),
-                          ),
-                        ]),
-                      ]),
+                      child: Column(children:
+                        followingWidget
+                      ),
                     ),
                     SingleChildScrollView(
-                      child: Column(children: <Widget>[
-                        Stack(children: <Widget>[
-                          Container(
-                            height: 100,
-                            width: double.infinity,
-                            alignment: Alignment.centerLeft,
-                            padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
-                            child: CircleAvatar(
-                              radius: 40,
-                              backgroundImage: NetworkImage(
-                                  'https://avatars.githubusercontent.com/u/57818885?v=4'),
-                              backgroundColor: Colors.transparent,
-                            ),
-                          ),
-                          Container(
-                            height: 100,
-                            width: double.infinity,
-                            child: SizedBox(
-                                width: double.infinity,
-                                height: double.infinity,
-                                child: TextButton(
-                                  style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty
-                                        .resolveWith<Color>(
-                                      (Set<MaterialState> states) {
-                                        if (states.contains(
-                                                MaterialState.pressed) ||
-                                            states.contains(
-                                                MaterialState.hovered))
-                                          return const Color(0x10000000);
-                                        return Colors.transparent;
-                                      },
-                                    ),
-                                  ),
-                                  onPressed: () {},
-                                  child: Container(
-                                    height: 100,
-                                    width: double.infinity,
-                                    alignment: Alignment.centerLeft,
-                                    padding: EdgeInsets.fromLTRB(100, 8, 4, 8),
-                                    child: RichText(
-                                      text: TextSpan(
-                                        text: 'friend2',
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontWeight: FontWeight.w700,
-                                            fontSize: 16.0),
-                                      ),
-                                    ),
-                                  ),
-                                )),
-                          ),
-                          Visibility(
-                            visible: false,
-                            child: Container(
-                              height: 100,
-                              width: double.infinity,
-                              alignment: Alignment.centerRight,
-                              padding: EdgeInsets.fromLTRB(4, 8, 15, 8),
-                              child: TextButton(
-                                style: TextButton.styleFrom(
-                                  textStyle: const TextStyle(fontSize: 16),
-                                  fixedSize: Size.fromHeight(50),
-                                ),
-                                onPressed: () {},
-                                child: const Text('Follow'),
-                              ),
-                            ),
-                          ),
-                          Visibility(
-                            visible: true,
-                            child: Container(
-                              height: 100,
-                              width: double.infinity,
-                              alignment: Alignment.centerRight,
-                              padding: EdgeInsets.fromLTRB(4, 8, 15, 8),
-                              child: TextButton(
-                                style: TextButton.styleFrom(
-                                  textStyle: const TextStyle(fontSize: 16),
-                                  fixedSize: Size.fromHeight(50),
-                                ),
-                                onPressed: () {
-                                  setState(() {});
-                                },
-                                child: Text('Unfollow',
-                                    style: TextStyle(color: Colors.red)),
-                              ),
-                            ),
-                          ),
-                        ]),
-                      ]),
+                      child: Column(children:
+                        followerWidget
+                      ),
                     ),
                   ],
                 ),
---
+++ lib/model/follows_model.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:typed_data';
+
+class Followers {
+  final List<dynamic> list;
+
+  const Followers({required this.list});
+
+  factory Followers.fromJson(Map<String, dynamic> json) {
+    return Followers(
+      list: json['followed_users'],
+    );
+  }
+}
+
+Future<dynamic> getUserData(String UserID) async{
+  final response = await http.get(Uri.parse('http://127.0.0.1:3000/getUserByID/$UserID'));
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Failed to load followed users');
+  }
+}
+Future<List> fetchFollowers(String UserID) async {
+  final response = await http.get(Uri.parse('http://127.0.0.1:3000/getFollowers/$UserID'));
+  var imglist = [];
+  if (response.statusCode == 200) {
+    Followers json = Followers.fromJson(jsonDecode(response.body));
+    for (var element in json.list){
+      final img = await http.get(Uri.parse('http://127.0.0.1:3000/getPPByID/${element["userId"]}'));
+      imglist.add(img.bodyBytes);
+    }
+    return [json,imglist];
+  } else {
+    throw Exception('Failed to load followed users');
+  }
+
+}
+
+Future<List> fetchFollowing(String UserID) async {
+  final response = await http.get(Uri.parse('http://127.0.0.1:3000/getFollowedUsers/$UserID'));
+  var imglist = [];
+  if (response.statusCode == 200) {
+    Followers json = Followers.fromJson(jsonDecode(response.body));
+    for (var element in json.list){
+      final img = await http.get(Uri.parse('http://127.0.0.1:3000/getPPByID/${element["userId"]}'));
+      imglist.add(img.bodyBytes);
+    }
+    return [json,imglist];
+  } else {
+    throw Exception('Failed to load followed users');
+  }
+}
+
+Future<Uint8List> fetchPhoto(String UserID) async{
+  final response = await http.get(Uri.parse('http://127.0.0.1:3000/getPPByID/$UserID'));
+  if (response.statusCode == 200) {
+    return response.bodyBytes;
+  } else {
+    throw Exception('Failed to load followed users');
+  }
+}
