79d6e80f8a037f6dfb9991eca35b2f031dca018d
Joel <triwira.joel@gmail.com>
2022-03-30 20:34:45 +0700

    fixing home api

--- lib/model/home.dart
+++ lib/model/home.dart
@@ -1,16 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class Feed {
-  final int userID;
-  final String username;
+class Timeline {
+  final List<dynamic> list;
 
-  const FollowedUser({required this.userID, required this.username});
+  const Timeline({required this.list});
 
-  factory FollowedUser.fromJson(Map<String, dynamic> json) {
-    return FollowedUser(
-      userID: json['userID'],
-      username: json['username'],
+  factory Timeline.fromJson(Map<String, dynamic> json) {
+    return Timeline(
+      list: json['timeline_data'],
     );
   }
 }
@@ -20,43 +18,21 @@ Future<dynamic> getUserData(String UserID) async{
   if (response.statusCode == 200) {
     return jsonDecode(response.body);
   } else {
-    throw Exception('Failed to load Feed');
+    throw Exception('Failed to load user data');
   }
 }
 
-Future<String> getTripsByUser(int UserID) async {
-  final response = await http.get(Uri.parse('http://127.0.0.1:3000/getTripDetailByUser/$UserID'));
+Future<dynamic> getTimeline(String UserID) async{
+  final response = await http.get(Uri.parse('http://127.0.0.1:3000/getTimeline/$UserID'));
+  var imglist = [];
   if (response.statusCode == 200) {
-    return jsonDecode(response.body);
+    Timeline json = Timeline.fromJson(jsonDecode(response.body));
+    for (var element in json.list) {
+      final img = await http.get(Uri.parse('http://127.0.0.1:3000/getEventPictureByID/${element["eventID"]}'));
+      imglist.add(img.bodyBytes);
+    }
+    return [json, imglist];
   } else {
-    throw Exception('Failed to load Feed');
-  }
-  });
-
-Future<String> getTripsImage(int tripID) async {
-  final response = await http.get(Uri.parse('http://127.0.0.1:3000/getTripsImage/$tripID'));
-  if (response.statusCode == 200) {
-    return jsonDecode(response.body);
-  } else {
-    throw Exception('Failed to load Feed');
-  }
-  });
-
-  //print(body);
-
-  final response = await http.post(
-    Uri.parse('http://127.0.0.1:3000/getTripDetailByID/:id'),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-      'Accept': 'application/json',
-    },
-    body: body,
-  );
-
-  if (response.statusCode == 201) {
-    return "SUCCESS";
-  } else {
-    // Error
-    return "ERROR";
+    throw Exception('Failed to load timeline data');
   }
 }
