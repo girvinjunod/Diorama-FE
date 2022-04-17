8272f32ef4b9030d057655c4088fb3d5bc2a1b1a
Melita <13519063@std.stei.itb.ac.id>
2022-04-17 11:05:22 +0700

    merge profile

---
+++ lib/model/profile.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+import 'package:diorama_id/main.dart';
+import 'package:http/http.dart' as http;
+
+class Profile {
+  final int userID;
+  final String username;
+  final String name;
+  final String email;
+
+  const Profile(
+      {required this.userID,
+      required this.username,
+      required this.name,
+      required this.email});
+
+  factory Profile.fromJson(Map<String, dynamic> json) {
+    return Profile(
+      userID: json['id'],
+      username: json['username'],
+      name: json['name'],
+      email: json['email'],
+    );
+  }
+}
+
+class Trips {
+  final List<dynamic> list;
+
+  const Trips({required this.list});
+
+  factory Trips.fromJson(dynamic json) {
+    return Trips(
+      list: json['tripIds'],
+    );
+  }
+}
+
+class TripDetail {
+  final int UserID;
+  final String StartDate;
+  final String EndDate;
+  final String TripName;
+  final String LocationName;
+
+  const TripDetail(
+      {required this.UserID,
+      required this.StartDate,
+      required this.EndDate,
+      required this.TripName,
+      required this.LocationName});
+
+  factory TripDetail.fromJson(Map<String, dynamic> json) {
+    return TripDetail(
+      UserID: json['userId'],
+      StartDate: json['startDate'],
+      EndDate: json['endDate'],
+      TripName: json['tripName'],
+      LocationName: json['locationName'],
+    );
+  }
+}
+
+Future<dynamic> getUser(String UserID) async {
+  var header = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Bearer ${Holder.token}',
+  };
+  
+  final response =
+      await http.get(Uri.parse('https://diorama-id.herokuapp.com/getUserByID/$UserID'), headers: header);
+  final img =
+      await http.get(Uri.parse('https://diorama-id.herokuapp.com/getPPByID/$UserID'), headers: header);
+  if (response.statusCode == 200 && img.statusCode == 200) {
+    dynamic res = Profile.fromJson(jsonDecode(response.body));
+    // print(res);
+
+    return [res, img.bodyBytes];
+  } else {
+    throw Exception('Failed to load user data');
+  }
+}
+
+Future<dynamic> getUserPP(String UserID) async {
+  var header = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Bearer ${Holder.token}',
+  };
+
+  final response =
+      await http.get(Uri.parse('https://diorama-id.herokuapp.com/getPPByID/$UserID'), headers: header);
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Failed to load user profpic');
+  }
+}
+
+Future<dynamic> getProfile(String UserID) async {
+  final user = await getUser(UserID);
+  final pp = await getUserPP(UserID);
+  return [user, pp];
+}
+
+Future<dynamic> getTripFromUser(String UserID) async {
+  var header = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Bearer ${Holder.token}',
+  };
+
+  final response =
+      await http.get(Uri.parse('https://diorama-id.herokuapp.com/getTripsByUser/$UserID'), headers: header);
+  // print(response.body.runtimeType);
+  var imgTripList = [];
+  var tripList = [];
+  if (response.statusCode == 200) {
+    Trips json = Trips.fromJson(jsonDecode(response.body));
+    for (var element in json.list) {
+      final img = await http
+          .get(Uri.parse('https://diorama-id.herokuapp.com/getTripsImage/$element'), headers: header);
+      imgTripList.add(img.bodyBytes);
+      final detail = await http
+          .get(Uri.parse('https://diorama-id.herokuapp.com/getTripDetailByID/$element'), headers: header);
+      tripList.add(TripDetail.fromJson(jsonDecode(detail.body)));
+    }
+    return [tripList, imgTripList];
+  } else {
+    throw Exception('Failed to load trips');
+  }
+}
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,6 +1,9 @@
+import 'package:diorama_id/detail_trip.dart';
+import 'package:diorama_id/edit_profile.dart';
 import 'package:diorama_id/main.dart';
 import 'package:flutter/material.dart';
-import 'edit_profile.dart';
+import 'package:diorama_id/model/profile.dart';
+import 'model/follows_model.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -10,11 +13,37 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  int _userID = 1;
+  // late Profile profile;
+  String _username = "";
+  var _trips = [];
+  var _tripPictures = [];
+  var followings = [];
+  late Followers _followingList;
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchFollowing(_userID.toString()).then((list) {
+      _followingList = list[0];
+      // print(_followingList.list);
+      setState(() {});
+    });
+
+    // if(FirebaseAuth.instance.currentUser() != null){
+
+    //   Navigator.of(context).pushReplacement(MaterialPageRoute(
+    //     builder: (context) => HomeScreen()
+    //   ));
+    // }
+  }
+
   // Apakah username berbeda dari name user?
   bool _isUsernameVisible = true;
 
   // Apakah profile milik sendiri?
-  bool _isSelfProfile = true;
+  bool _isSelfProfile = false;
 
   // Apakah user mem-follow user ini?
   bool _isFollowed = false;
@@ -25,28 +54,67 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF1F1F1),
-      body: ListView(shrinkWrap: true, children: <Widget>[
+        backgroundColor: const Color(0xFFF1F1F1),
+        body: FutureBuilder(
+          future: getUser(_userID.toString()),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                  child: Text("Loading..."),
+                ),
+              );
+            } else {
+              // Profile a = snapshot.data[0];
+              // print(a);
+              // return Text(a.name);
+              return profileView(context, snapshot);
+            }
+          },
+        ));
+  }
+
+  Widget profileView(BuildContext context, AsyncSnapshot snapshot) {
+    Profile _profile = snapshot.data[0];
+    var _profilepp = snapshot.data[1];
+    if (_profile.name == _profile.username) {
+      _isUsernameVisible = false;
+    }
+    if (_profile.userID == _userID) {
+      _isSelfProfile = true;
+    }
+    // if (_followingList.list["userID"].contains(_profile.userID)) {
+    //   _isFollowed = true;
+    // }
+    return ListView(
+      shrinkWrap: true,
+      children: <Widget>[
         Container(
           color: const Color(0xFFD4F1F4),
           child: Column(
             children: [
-              SizedBox(height: 40),
+              SizedBox(
+                height: 40,
+              ),
               CircleAvatar(
-                radius: 100.0,
-                backgroundImage: NetworkImage(
-                    'https://avatars.githubusercontent.com/u/57818885?v=4'),
+                radius: 100,
+                backgroundImage: MemoryImage(_profilepp),
                 backgroundColor: Colors.transparent,
               ),
-              SizedBox(height: 20),
+              SizedBox(
+                height: 20,
+              ),
               Text(
-                'Nama Pengguna',
-                style: TextStyle(fontSize: 24),
+                _profile.username,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               SizedBox(height: 3),
               Visibility(
                 child: Text(
-                  '(username_jika_beda)',
+                  _profile.username,
                   style: TextStyle(fontSize: 16),
                 ),
                 visible: _isUsernameVisible,
@@ -172,237 +240,133 @@ class _ProfilePageState extends State<ProfilePage> {
                 child: SizedBox(height: 20),
                 visible: _noTrips,
               ),
-              Visibility(
-                child: Stack(children: <Widget>[
-                  Container(
-                    height: 150,
-                    width: double.infinity,
-                    child: Image.network(
-                      'https://drive.google.com/uc?export=view&id=1DMtJUYp6U2F2FXDkGCdfJOno0k_0RcG6',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Container(
-                    height: 150,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          const Color(0x02000000),
-                          const Color(0x90000000),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 150,
-                    width: double.infinity,
-                    child: SizedBox(
-                        width: double.infinity,
-                        height: double.infinity, // <-- match_parent
-                        child: TextButton(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.resolveWith<Color>(
-                              (Set<MaterialState> states) {
-                                if (states.contains(MaterialState.pressed) ||
-                                    states.contains(MaterialState.hovered))
-                                  return const Color(0x70000000);
-                                return Colors
-                                    .transparent; // Use the component's default.
-                              },
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Container(
-                            height: 150,
-                            width: double.infinity,
-                            alignment: Alignment.bottomLeft,
-                            padding: EdgeInsets.fromLTRB(4, 8, 4, 8),
-                            child: RichText(
-                              text: TextSpan(
-                                text:
-                                    'jalan-jalan di planet bumi sebagai manusia biasa\n',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 22.0),
-                                children: const <TextSpan>[
-                                  TextSpan(
-                                      text: '16 Feb 2022 - 21 Feb 2022\n',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12.0,
-                                          height: 1.5)),
-                                  TextSpan(
-                                      text:
-                                          'Tokyo, Japan, Asia, Planet Bumi, Galaksi Milky Way',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12.0,
-                                          height: 1.5)),
-                                ],
-                              ),
-                            ),
-                          ),
-                        )),
-                  ),
-                ]),
-                visible: !_noTrips,
-              ),
-              Visibility(
-                child: Stack(children: <Widget>[
-                  Container(
-                    height: 150,
-                    width: double.infinity,
-                    child: Image.network(
-                      'https://drive.google.com/uc?export=view&id=1EnkLO8V868NWgzcbmy-6OTGtkHXMPINF',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Container(
-                    height: 150,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          const Color(0x02000000),
-                          const Color(0x90000000),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 150,
-                    child: SizedBox(
-                        width: double.infinity,
-                        height: double.infinity, // <-- match_parent
-                        child: TextButton(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.resolveWith<Color>(
-                              (Set<MaterialState> states) {
-                                if (states.contains(MaterialState.pressed) ||
-                                    states.contains(MaterialState.hovered))
-                                  return const Color(0x70000000);
-                                return Colors
-                                    .transparent; // Use the component's default.
-                              },
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Container(
-                            height: 150,
-                            width: double.infinity,
-                            alignment: Alignment.bottomLeft,
-                            padding: EdgeInsets.fromLTRB(4, 8, 4, 8),
-                            child: RichText(
-                              text: TextSpan(
-                                text:
-                                    'sekolah rantau di negara sebelah, taunya malah kiamat (real)\n',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 22.0),
-                                children: const <TextSpan>[
-                                  TextSpan(
-                                      text: '10 May S.1206 - 26 Aug S.1206\n',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12.0,
-                                          height: 1.5)),
-                                  TextSpan(
-                                      text: 'Leeves, Erebonia, Zemuria',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12.0,
-                                          height: 1.5)),
-                                ],
-                              ),
-                            ),
-                          ),
-                        )),
-                  )
-                ]),
-                visible: !_noTrips,
-              ),
-              Visibility(
-                child: Stack(children: <Widget>[
-                  Container(
-                    height: 150,
-                    width: double.infinity,
-                    child: Image.network(
-                      'https://drive.google.com/uc?export=view&id=1dcKb4FWicycUGU50RHdfqOe-68hRhxQP',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Container(
-                    height: 150,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          const Color(0x02000000),
-                          const Color(0x90000000),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 150,
-                    child: SizedBox(
-                        width: double.infinity,
-                        height: double.infinity, // <-- match_parent
-                        child: TextButton(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.resolveWith<Color>(
-                              (Set<MaterialState> states) {
-                                if (states.contains(MaterialState.pressed) ||
-                                    states.contains(MaterialState.hovered))
-                                  return const Color(0x70000000);
-                                return Colors
-                                    .transparent; // Use the component's default.
-                              },
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Container(
-                            height: 150,
-                            width: double.infinity,
-                            alignment: Alignment.bottomLeft,
-                            padding: EdgeInsets.fromLTRB(4, 8, 4, 8),
-                            child: RichText(
-                              text: TextSpan(
-                                text: '2 jam di rumah ga ngapa-ngapain\n',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 22.0),
-                                children: const <TextSpan>[
-                                  TextSpan(
-                                      text: '14 Feb 2022 - 14 Feb 2022\n',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12.0,
-                                          height: 1.5)),
-                                  TextSpan(
-                                      text: 'Depan Laptop',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12.0,
-                                          height: 1.5)),
-                                ],
-                              ),
-                            ),
-                          ),
-                        )),
-                  )
-                ]),
-                visible: !_noTrips,
-              ),
-            ]))
-      ]),
+            ])),
+        Container(
+          child: tripListView(context, snapshot),
+        ),
+      ],
     );
+  }
+
+  Widget tripListView(BuildContext context, AsyncSnapshot snap) {
+    return Container(
+      child: FutureBuilder(
+          future: getTripFromUser(_userID.toString()),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Visibility(
+                child: Container(
+                  child: Center(
+                    child: Text("No Trips Available"),
+                  ),
+                ),
+                visible: _noTrips,
+              );
+            } else {
+              // print(snap.data);
+              // print(snapshot.data);
+              return tripView(context, snapshot);
+            }
+          }),
+    );
+  }
+
+  Widget tripView(BuildContext context, AsyncSnapshot snapshot) {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemCount: snapshot.data[0].length,
+      itemBuilder: (context, index) {
+        return tripCard(context, snapshot, index);
+      },
+    );
+  }
+
+  Widget tripCard(BuildContext context, AsyncSnapshot snapshot, int index) {
+    return Stack(
+      children: <Widget>[
+        _tripPic(context, snapshot, index),
+        _tripText(context, snapshot, index),
+      ],
+    );
+  }
+
+  Widget _tripPic(BuildContext context, AsyncSnapshot snapshot, int index) {
+    var _img = snapshot.data[1][index];
+    // print(snapshot.data[1][1]);
+    if (snapshot.data[1][index].length == 0) {
+      return Container(
+        child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text("No Image"),
+        ])),
+      );
+    } else {
+      return Container(
+          height: 150,
+          width: double.infinity,
+          child: Image.memory(
+            _img,
+            fit: BoxFit.cover,
+          ));
+    }
+  }
+
+  Widget _tripText(BuildContext context, AsyncSnapshot snapshot, int index) {
+    dynamic _tripName = snapshot.data[0][index].TripName;
+    dynamic _tripStartDate = snapshot.data[0][index].StartDate;
+    dynamic _tripEndDate = snapshot.data[0][index].EndDate;
+    dynamic _tripLocation = snapshot.data[0][index].LocationName;
+    return Container(
+        height: 150,
+        child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed) ||
+                        states.contains(MaterialState.hovered))
+                      return const Color(0x70000000);
+                    return Colors.transparent; // Use the component's default.
+                  },
+                )),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        // masih hardcode, fix later
+                        builder: (context) => DetailTripPage(1, _userID)),
+                  );
+                },
+                child: Container(
+                    height: 150,
+                    width: double.infinity,
+                    alignment: Alignment.bottomLeft,
+                    padding: EdgeInsets.fromLTRB(4, 8, 4, 8),
+                    child: RichText(
+                      text: TextSpan(
+                        text: '$_tripName\n',
+                        style: TextStyle(color: Colors.white, fontSize: 22),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: '$_tripStartDate - $_tripEndDate\n',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                height: 1.5,
+                              )),
+                          TextSpan(
+                              text: '$_tripLocation\n',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                height: 1.5,
+                              )),
+                        ],
+                      ),
+                    )))));
   }
 }
